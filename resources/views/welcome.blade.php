0a5f7d1fa9f36c7c0e7933e17186f2ee132da7dd
abror <abror.eshkabilov@gmail.com>
2020-08-19 20:53:34 +0500

    some changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    KhanTech
                 </div>
 
                 <div class="links">
