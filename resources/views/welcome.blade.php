cd379ff7f1f49735da8f767c2d5a8c4babca144d
abror <abror.eshkabilov@gmail.com>
2020-08-19 21:36:15 +0500

    latest

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    KhanTech
+                    KhanTech group
                 </div>
 
                 <div class="links">
